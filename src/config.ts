c8994884d8fe409644eeeea4877d174e8f826004
Octavian Regatun <octavian.regatun@outlook.com>
2020-11-25 12:09:48 +0200

    update: mongooseURL in case of test env

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-export const mongooseURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.bcluj.mongodb.net/main?retryWrites=true&w=majority`;
+const database = process.env.TEST == 'true' ? 'test' : 'main';
+export const mongooseURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.bcluj.mongodb.net/${database}?retryWrites=true&w=majority`;
 export const dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 };
-
